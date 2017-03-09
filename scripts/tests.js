62efa5fbeb91ef1b547b6ea5cd46fa9aa1bacb69
NikitaSadinidhiVijayan <sv.nikita91@yahoo.com>
2017-03-08 21:14:54 -0800

    moved to repo
    fixed all issues

---
+++ scripts/tests.js
@@ -0,0 +1,53 @@
+/*global QUnit */
+/*
+The issue is with console.log function when trying to run Qunit test scripts on truck.js
+To avoid this problem, i have created a seperate function for handling the console.log
+This function returns the string which it supposed to print.
+*/
+QUnit.test('check datastore', function(assert) {
+    var ds = new window.App.DataStore();
+    ds.add('m@bond.com', 'tea');
+    ds.add('james@bond.com', 'eshpressho');
+    //assert.equal(ds.add('m@bond.com', 'tea'), undefined);
+    assert.equal(ds.add('james@bond.com', 'eshpressho'), undefined);
+    assert.ok(ds.getAll(), 'returns value');
+    //assert.equal(ds.getAll(), 'returns value');
+    assert.deepEqual(ds.getAll(), {
+        'm@bond.com': 'tea',
+        'james@bond.com': 'eshpressho'
+    }, 'getall returns correct value');
+    ds.remove('james@bond.com');
+    assert.ok(ds.getAll(), 'returns value');
+    assert.equal(ds.get('m@bond.com'), 'tea');
+    assert.equal(ds.get('james@bond.com'), undefined);
+
+});
+QUnit.test('check truckjs', function(assert) {
+    var myTruck = window.myTruck;
+
+    assert.equal(myTruck.createOrder({
+        emailAddress: 'me@goldfinger.com',
+        coffee: 'double mocha'
+    }), 'Adding order for me@goldfinger.com');
+    assert.equal(myTruck.createOrder({
+        emailAddress: 'dr@no.com',
+        coffee: 'decaf'
+    }), 'Adding order for dr@no.com');
+    assert.equal(myTruck.createOrder({
+        emailAddress: 'm@bond.com',
+        coffee: 'earl grey'
+    }), 'Adding order for m@bond.com');
+    var arr = myTruck.printOrders();
+
+    assert.equal(arr[0], 'Truck #ncc-1701 has pending orders:');
+
+    assert.equal((arr.length) - 1, 3, 'Passed- printed 3 orders');
+
+    assert.equal(myTruck.deliverOrder('dr@no.com'), 'Delivering order for dr@no.com');
+    assert.equal(myTruck.deliverOrder('m@bond.com'), 'Delivering order for m@bond.com');
+    assert.deepEqual(myTruck.printOrders(), ['Truck #ncc-1701 has pending orders:', {
+        'emailAddress': 'me@goldfinger.com',
+        'coffee': 'double mocha'
+    }]);
+
+});
---
+++ scripts/truck.js
@@ -0,0 +1,41 @@
+(function(window) {
+    'use strict';
+    var App = window.App || {};
+
+    function Truck(truckId, db) {
+        this.truckId = truckId;
+        this.db = db;
+    }
+
+    function consoleprint(str) {
+        console.log(str);
+        return str;
+    }
+
+    Truck.prototype.createOrder = function(order) {
+        // modified for testing
+        var res1 = consoleprint('Adding order for ' + order.emailAddress);
+        this.db.add(order.emailAddress, order);
+        return res1;
+    };
+    Truck.prototype.deliverOrder = function(customerId) {
+        var res2 = consoleprint('Delivering order for ' + customerId);
+        this.db.remove(customerId);
+        return res2;
+    };
+    Truck.prototype.printOrders = function() {
+        var customerIdArray = Object.keys(this.db.getAll());
+        var out_arr = [];
+        var res3 = consoleprint('Truck #' + this.truckId + ' has pending orders:');
+        out_arr.push(res3);
+        customerIdArray.forEach(function(id) {
+            res3 = consoleprint(this.db.get(id));
+            out_arr.push(res3);
+
+        }.bind(this));
+        return out_arr; //for testing purpose
+    };
+    App.Truck = Truck;
+    window.App = App;
+
+})(window);
